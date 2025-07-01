00fd365219f3bdda338b8f8e95efa679f3a7d8e2
root <root@bunsen.bunsen>
2025-06-30 23:25:53 -0500

    Revirtiendo cambios hasta el commit e9045c5

--- src/app/components/one/one.component.ts
+++ src/app/components/one/one.component.ts
@@ -127,10 +127,6 @@ export class OneComponent implements OnInit, AfterViewInit {
     }
   }
 
-  hasOriginAndDestination(): boolean {
-    return !!this.origin && !!this.destination;
-  }
-
   resetForm(): void {
     // Clear the form and submission state
     this.clearSubmissionState();
